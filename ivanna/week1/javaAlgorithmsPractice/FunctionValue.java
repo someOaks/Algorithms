b5bfe096b1fc5fa3f901fbdf6e73d2cadbac2bbc
Ivanna <ivanna.sak@gmail.com>
2019-01-09 21:22:54 -0800

    percolation assignment

---
+++ ivanna/week1/javaAlgorithmsPractice/FunctionValue.java
@@ -0,0 +1,25 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+package javaAlgorithmsPractice;
+
+import edu.princeton.cs.algs4.StdDraw;
+
+public class FunctionValue {
+    public static void main(String[] args) {
+        int N = 100;
+        StdDraw.setXscale(0, N);
+        StdDraw.setYscale(0, N*N);
+        StdDraw.setPenRadius(.01);
+
+        for (int i = 1; i <= N; i++ ){
+            StdDraw.point(i, i);
+            StdDraw.point(i, i*i);
+            StdDraw.point(i, i*Math.log(i));
+        }
+
+    }
+}
