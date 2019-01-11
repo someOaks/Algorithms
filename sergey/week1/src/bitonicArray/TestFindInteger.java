fbe818da590f7817da42a07bcb973fff471ea494
seralekseenko <seralekseenko@ukr.net>
2019-01-11 08:43:20 +0200

    #32 fix bugs in tests.

--- sergey/week1/src/bitonicArray/TestFindInteger.java
+++ sergey/week1/src/bitonicArray/TestFindInteger.java
@@ -1,10 +1,12 @@
 package bitonicArray;
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Stopwatch;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class TestFindInteger {
     private static int[] array1 = {-10, -7, -2, 0, 1, 3, 5, 8, 7, 6, 4, 2, -1, -3}; // center
@@ -13,7 +15,7 @@ public class TestFindInteger {
     private static int[] array4 = {-10, -7, -2, 0, 1, 3, 5, 8}; // Возрастание
     private static int[] array5 = {-7, -6, -4, -2, 1, 2, 4, 6, 8, 7, 5, 3, 0, -1, -3, -5, -8, -12}; // хитровыебаный
 
-    private final static int NUMBER_OF_TESTS = 10;
+    private final static int NUMBER_OF_TESTS = 250;
 
 
 
@@ -29,7 +31,6 @@ public class TestFindInteger {
 
 
 
-        /*
         int[] fromFile =  new int[BitonicArrayGenerator.ARRAY_SIZE];
         In in = new In(BitonicArrayGenerator.FILE_NAME);
         for (int i = 0; i < fromFile.length; i++) {
@@ -37,33 +38,22 @@ public class TestFindInteger {
         }
         in.close();
 
-        testBitonicArrayFromFile(fromFile, fromFile[new Random().nextInt(BitonicArrayGenerator.ARRAY_SIZE)]);*/
+        testBitonicArrayFromFile(fromFile);
     }
 
 
     private static void testBitonicArrayFromFile(int[] a) {
         System.out.println("## testBitonicArrayFromFile ## KEY: random");
-    
-        int b;
-        Double[] scores = new Double[NUMBER_OF_TESTS];
+        Random random = new Random();
+        Double[] scores;
         FindIntegerInBitonicArray finder = new FindIntegerInBitonicArray(a);
-        
-        for (int i = 0; i < NUMBER_OF_TESTS; i++) {
-            b = BitonicArrayGenerator.generateRandomInt();
-            Stopwatch stopwatch = new Stopwatch();
-            int c = finder.correctSolution(b);
-            Double time = stopwatch.elapsedTime();
-            scores[i] = time;
-//            System.out.println("Correct key index = " + c + "Time: " + time);
-        }
-
-        printScore(scores);
 
         scores = new Double[NUMBER_OF_TESTS];
         for (int i = 0; i < NUMBER_OF_TESTS; i++) {
-            b = BitonicArrayGenerator.generateRandomInt();
+            int key = random.nextInt(BitonicArrayGenerator.ARRAY_SIZE);
             Stopwatch stopwatch = new Stopwatch();
-            int g = finder.findKeyIndex(b);
+            int g = finder.findKeyIndex(key);
+//            System.out.println(g);
             Double time = stopwatch.elapsedTime();
             scores[i] = time;
 //            System.out.println("My method return key index = " + g + "Time: " + time);
