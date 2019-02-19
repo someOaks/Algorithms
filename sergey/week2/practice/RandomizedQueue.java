71fc178483915fffeea420e8d671c0b22e159ebd
seralekseenko <seralekseenko@ukr.net>
2019-02-19 21:39:07 +0200

    Finish RandomizeQueue on linked platform.

--- sergey/week2/practice/RandomizedQueue.java
+++ sergey/week2/practice/RandomizedQueue.java
@@ -1,29 +1,35 @@
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
     private int size = 0;
 
-    private Node lastAddedItem;
+    private Node lastAddedNode, firstNode;
 
     private class Node {
         private final Item item;
-        private final Node next;
+        private  Node next, previous;
 
-        public Node(Item item, Node next) {
+        public Node(Item item,  Node previous) {
             this.item = item;
-            this.next = next;
+            this.previous = previous;
         }
     }
 
 
     public RandomizedQueue() { // construct an empty randomized queue
-        this.lastAddedItem = null;
+        this.lastAddedNode = null;
     }
 
     public boolean isEmpty() { // is the randomized queue empty?
         return size == 0;
     }
 
+    /**
+     * @return number of items in the RandomizedQueue.
+     */
     public int size() { // return the number of items on the randomized queue
         return size;
     }
@@ -35,9 +41,14 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     public void enqueue(Item item) { // add the item
         if (item == null) throw new IllegalArgumentException();
 
-        Node tempItem = lastAddedItem;
-
-        lastAddedItem = new Node(item, tempItem);
+        if (isEmpty()) {
+            lastAddedNode = new Node(item,  null);
+            firstNode = lastAddedNode;
+        } else {
+            Node tempNode = lastAddedNode;
+            lastAddedNode = new Node(item, tempNode);
+            tempNode.next = lastAddedNode;
+        }
         size++;
     }
 
@@ -46,17 +57,124 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
      * @return removed random item from RandomizedQueue.
      */
     public Item dequeue() { // remove and return a random item
-        return null;
+        if (isEmpty()) throw new NoSuchElementException("RandomizedQueue is empty!");
+
+        Node randomNode = getRandomNode();
+
+        if (size == 1) {
+            firstNode = null;
+            lastAddedNode = null;
+        }
+
+        if (randomNode.previous != null) {
+            randomNode.previous.next = randomNode.next;
+        }
+
+        if (randomNode.next != null) {
+            randomNode.next.previous = randomNode.previous;
+        }
+
+        size--;
+        return randomNode.item;
     }
+
+    /**
+     * Searches for and returns a random item in the queue.
+     * @return - item in uniformly random order.
+     */
     public Item sample() { // return a random item (but do not remove it)
-        return null;
+        if (isEmpty()) throw new NoSuchElementException("RandomizedQueue is empty!");
+
+        return getRandomNode().item;
     }
+
+    /**
+     * Using a pseudo-random number generator, a node is selected from among
+     * those present in the RandomizedQueue.
+     * @return - node in uniformly random order.
+     */
+    private Node getRandomNode() {
+        int randoomInt = StdRandom.uniform(size) + 1;
+        System.out.println("RANOMint: " + randoomInt);
+        Node result = firstNode;
+
+        if (randoomInt < (size/2)) {
+            for (int i = 1; i < randoomInt; i++) {
+                result  = result.next;
+            }
+        }
+
+        if (randoomInt > (size/2)) {
+            result = lastAddedNode;
+            for (int i = size; i > randoomInt; i--) {
+                result = result.previous;
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Returns a fully independent iterator, passing it all the current elements
+     * of the RandomizedQueue.
+     * @return - RandomIterator.
+     */
     public Iterator<Item> iterator() { // return an independent iterator over items in random order
-        return null;
+        Item[] itemArray = (Item[])new Object[size];
+        Node currentNode = firstNode;
+
+        for (int i = 0; i < itemArray.length; i++) {
+            itemArray[i] = currentNode.item;
+            currentNode = currentNode.next;
+        }
+        return new RandomIterator<Item>(itemArray);
     }
+
+    /**
+     * Independent iterator from the surrounding class. After receiving this
+     * iterator, the surrounding class can be destroyed.
+     */
+    private static class RandomIterator<Item> implements Iterator<Item> {
+
+        private int currentIndex = 0;
+
+        private Item[] randomOrderItems;
+
+        private RandomIterator(Item[] itemArray) {
+            StdRandom.shuffle(itemArray);
+            randomOrderItems = itemArray;
+        }
+
+        /**
+         * Returns {@code true} if the iteration has more elements. (In other words, returns {@code
+         * true} if {@link #next} would return an element rather than throwing an exception.)
+         *
+         * @return {@code true} if the iteration has more elements
+         */
+        @Override
+        public boolean hasNext() {
+            return currentIndex < randomOrderItems.length;
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if the iteration has no more elements
+         */
+        @Override
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException("No more elements fo iteration.");
+            return randomOrderItems[currentIndex++];
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Ленинград - дорожная");
+        }
+    }
+
     public static void main(String[] args) { // unit testing (optional)
 
     }
-
-
 }
--- sergey/week2/practice/TestRandomizedQueue.java
+++ sergey/week2/practice/TestRandomizedQueue.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class TestRandomizedQueue {
 
@@ -19,14 +21,133 @@ public class TestRandomizedQueue {
     }
 
     @Test
-    public void dequeue() {
+    public void dequeueBig() {
+        RandomizedQueue<String> bagInt = new RandomizedQueue<>();
+
+        assertTrue(bagInt.isEmpty());
+        assertEquals(0, bagInt.size());
+
+        for (int i = 1; i < 22; i++) {
+            bagInt.enqueue(String.valueOf(i));
+        }
+        assertFalse(bagInt.isEmpty());
+        assertEquals(21, bagInt.size());
+
+        for (int i = 1; i < 22; i++) {
+            int number = Integer.parseInt(bagInt.dequeue());
+            System.out.println(number);
+
+            assertTrue("number is out of range: " + number, 1 <= number && number <= 21);
+        }
     }
 
     @Test
-    public void sample() {
+    public void dequeueLittle() {
+        RandomizedQueue<String> bagInt = new RandomizedQueue<>();
+
+        assertTrue(bagInt.isEmpty());
+        assertEquals(0, bagInt.size());
+
+        for (int i = 1; i < 3; i++) {
+            bagInt.enqueue(String.valueOf(i));
+        }
+        assertFalse(bagInt.isEmpty());
+        assertEquals(2, bagInt.size());
+
+        for (int i = 1; i < 3; i++) {
+            int number = Integer.parseInt(bagInt.dequeue());
+            System.out.println(number);
+
+            assertTrue("number is out of range: " + number, 1 <= number && number <= 2);
+        }
+
+        assertTrue(bagInt.isEmpty());
+        assertEquals(0, bagInt.size());
+
+        bagInt.enqueue(String.valueOf(45));
+
+        assertFalse(bagInt.isEmpty());
+        assertEquals(1, bagInt.size());
+        assertEquals(45, Integer.parseInt(bagInt.dequeue()));
     }
 
     @Test
-    public void iterator() {
+    public void sample21() {
+        RandomizedQueue<String> bagInt = new RandomizedQueue<>();
+
+        assertTrue(bagInt.isEmpty());
+        assertEquals(0, bagInt.size());
+
+        for (int i = 1; i < 22; i++) {
+            bagInt.enqueue(String.valueOf(i));
+        }
+        assertFalse(bagInt.isEmpty());
+        assertEquals(21, bagInt.size());
+
+        for (int i = 0; i < 100; i++) {
+            int number = Integer.parseInt(bagInt.sample());
+            System.out.println(number);
+
+            assertTrue("number is out of range: " + number, 1 <= number && number <= 21);
+        }
+    }
+
+    @Test
+    public void sampleMin() {
+        RandomizedQueue<String> bagInt = new RandomizedQueue<>();
+
+        assertTrue(bagInt.isEmpty());
+        assertEquals(0, bagInt.size());
+
+        bagInt.enqueue(String.valueOf(1));
+        assertFalse(bagInt.isEmpty());
+        assertEquals(1, bagInt.size());
+
+        bagInt.enqueue(String.valueOf(2));
+
+        for (int i = 0; i < 100; i++) {
+            int number = Integer.parseInt(bagInt.sample());
+            System.out.println(number);
+
+            assertTrue("number is out of range: " + number, 1 <= number && number <= 2);
+        }
+    }
+
+    @Test
+    public void iterator21() {
+        RandomizedQueue<String> bagInt = new RandomizedQueue<>();
+
+        assertTrue(bagInt.isEmpty());
+        assertEquals(0, bagInt.size());
+
+        for (int i = 1; i < 22; i++) {
+            bagInt.enqueue(String.valueOf(i));
+        }
+        assertFalse(bagInt.isEmpty());
+        assertEquals(21, bagInt.size());
+
+        int number;
+        for (String item: bagInt) {
+            number = Integer.parseInt(item);
+            assertTrue("number is out of range: " + number, 1 <= number && number <= 21);
+        }
+    }
+
+    @Test
+    public void iterator1() {
+        RandomizedQueue<String> bagInt = new RandomizedQueue<>();
+
+        assertTrue(bagInt.isEmpty());
+        assertEquals(0, bagInt.size());
+
+            bagInt.enqueue(String.valueOf(1));
+        assertFalse(bagInt.isEmpty());
+        assertEquals(1, bagInt.size());
+
+        int number;
+        for (String item: bagInt) {
+            number = Integer.parseInt(item);
+            assertTrue("number is out of range: " + number, 1 <= number && number <= 1);
+        }
     }
 }
