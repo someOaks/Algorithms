3af17ae92e65b34ba2d46a875e5f99dc5ede9857
Glebuar <glebuar@gmail.com>
2019-02-20 16:16:45 +0200

    Comments

---
+++ gleb/week5/practice/PointSET.java
@@ -0,0 +1,79 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+public class PointSET {
+
+    /**
+     * Construct an empty set of points.
+     */
+    public PointSET() {
+
+    }
+
+    /**
+     * Checks the set is empty.
+     * @return @code true} if setis empty; {@code false} otherwise.
+     */
+    public boolean isEmpty() {
+
+    }
+
+    /**
+     * Counts the number of points in the set.
+     * @return number of points in the set.
+     */
+    public int size() {
+
+    }
+
+    /**
+     * Add the point to the set (if it is not already in the set).
+     * @param p the point.
+     */
+    public void insert(Point2D p) {
+
+    }
+
+    /**
+     * Checks if the set contain point p.
+     * @param p the point.
+     * @return {@code true} if set contain point p; {@code false} otherwise.
+     */
+    public boolean contains(Point2D p) {
+
+    }
+
+    /**
+     * Draw all points to standard draw.
+     */
+    public void draw() {
+
+    }
+
+    /**
+     * all points that are inside the rectangle (or on the boundary)
+     * @param rect the rectangle.
+     * @return all points inside the rectangle.
+     */
+    public Iterable<Point2D> range(RectHV rect) {
+
+    }
+
+    /**
+     * A nearest neighbor in the set to point p; null if the set is empty.
+     * @param p the point.
+     * @return a nearest neighbor.
+     */
+    public Point2D nearest(Point2D p) {
+
+    }
+
+    /**
+     * Unit testing of the methods (optional).
+     * @param args the command-line arguments.
+     */
+    public static void main(String[] args) {
+
+    }
+}
+
